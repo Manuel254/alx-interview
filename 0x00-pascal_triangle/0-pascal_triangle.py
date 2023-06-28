4ef91357d3b7bbf0a17834c0dfbe63612ee17541
Manuel254 <Immanuelkituku@gmail.com>
2023-06-28 15:13:29 +0000

    Add script that generates the pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+"""Create a list of lists of integers representing
+the Pascal's triangle of `n`
+"""
+
+
+def pascal_triangle(n):
+    """Returns pascal triangle as a list of lists
+    of integers
+    """
+    if n <= 0:
+        return []
+
+    array = [[1]]
+
+    for i in range(n - 1):
+        new_array = [];
+        copy_array = array[i].copy()
+        copy_array.insert(0, 0)
+        copy_array.append(0)
+        for j in range(len(copy_array) - 1):
+            sum_arr = copy_array[j] + copy_array[j+1]
+            new_array.append(sum_arr)
+        array.append(new_array)
+    return array
