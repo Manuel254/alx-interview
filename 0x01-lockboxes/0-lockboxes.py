8d2e931f6fa5e52df311b4e9a25d0e2d353c2b74
Manuel254 <Immanuelkituku@gmail.com>
2023-07-10 20:43:40 +0300

    Fix pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,8 +3,8 @@
 """This is an interview Question called lockboxes that
 checks to see if all boxes can be unlocked.
 
-You have n number of locked boxes in front of you. 
-Each box is numbered sequentially from 0 to n - 1 and 
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1 and
 each box may contain keys to the other boxes.
 """
 
@@ -12,13 +12,13 @@ each box may contain keys to the other boxes.
 def canUnlockAll(boxes):
     stack = [0]
     visited = set(stack)
-    
+
     while stack:
         idx = stack.pop()
-        
+
         for j in boxes[idx]:
             if j not in visited:
                 stack.append(j)
                 visited.add(j)
-                
+
     return len(visited) == len(boxes)
