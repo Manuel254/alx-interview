9cca5dcad44bf6d4ad4e4feab29141a22d1c7b65
Manuel254 <Immanuelkituku@gmail.com>
2023-08-23 18:49:28 +0300

    Add documentation

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""This module contains solution to make change"""
 
 
 def makeChange(coins, total):
+    """Calculate fewest number of coins to meet a given
+    amount of total
+    Return:
+        count of coins
+        else:
+            0 if total is 0
+            -1 if total cannot be met by any number of coins
+    """
     count = 0
     if total <= 0:
         return 0
