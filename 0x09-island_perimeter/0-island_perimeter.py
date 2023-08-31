e59618b86c2c11021904193f0002ed0f0100115b
Manuel254 <Immanuelkituku@gmail.com>
2023-08-31 15:25:30 +0300

    Fix pycodestyle

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """This module contains solution to island perimeter problem"""
 
+
 def island_perimeter(grid):
     """"Island perimeter solution"""
     visited = set()
 
     def dfs(i, j):
         if i >= len(grid) or j >= len(grid[0]) or \
-            i < 0 or j < 0 or not grid[i][j]:
+             i < 0 or j < 0 or not grid[i][j]:
             return 1
         if (i, j) in visited:
             return 0
-        
+
         visited.add((i, j))
         perimeter = dfs(i, j + 1)
         perimeter += dfs(i + 1, j)
@@ -19,8 +20,10 @@ def island_perimeter(grid):
         perimeter += dfs(i - 1, j)
         return perimeter
 
+    if not len(grid) and not len(grid[0]):
+        return 0
+
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 return dfs(i, j)
-    
